022a7cb2373cad181e6c690abbb0f5156fe4d920
Dan Osborne <dan.osborne@rocketmail.com>
2017-12-22 11:42:33 +0000

    gulp build added

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var gulp        = require('gulp');
+var gbrowserify = require('gulp-browserify');
+
+gulp.task('browserify', function () {
+    gulp.src('./env/src/index.js', {entry: true})
+        .pipe(gbrowserify({
+            transform: ['babelify']
+        }))
+        .pipe(gulp.dest('./env/dist'))
+});
+
+gulp.task('watch', function () {
+    gulp.watch('./env/src/**/*.js', ['browserify']);
+});
+
+gulp.task('default',  ['browserify']);
