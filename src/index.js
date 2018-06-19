885b1a2c1b433c9423b56163568ab1c8602632f7
Dan Osborne <dan.osborne@rocketmail.com>
2018-06-19 10:36:13 +0100

    Refactored settings configuration

--- src/index.js
+++ src/index.js
@@ -8,59 +8,51 @@ class SvgDoughnut extends React.Component {
 
     constructor(props) {
         super(props);
-
         this.state = {
             appliedPercentage: 0
         };
-
-        this.styles = generateStyles(this.props.settings);
-        this.size = this.props.settings.size || 150;
-        this.percentage = this.props.percentage || 0;
-        this.animationDuration = this.props.settings.animationDuration;
-        this.labelText = this.props.settings.labelText;
         this.settings = configure(this.props.settings);
-        console.log(this.settings);
     }
 
     componentDidMount() {
-        if (this.animationDuration || this.animationDuration > 0) {
+        if (this.settings.animationDuration || this.settings.animationDuration > 0) {
             this.animate();
         } else {
             this.setState({
-                appliedPercentage: this.percentage
+                appliedPercentage: this.settings.percentage
             });
         }
     }
 
     animate() {
         const self = this;
-        for(let i = 0; i < (this.percentage + 1); i++){
+        for(let i = 0; i < (this.settings.percentage + 1); i++){
             setTimeout(() => {
                 self.setState({
                     appliedPercentage: i
                 });
-            }, i * stepDuration(this.percentage, this.animationDuration));
+            }, i * stepDuration(this.settings.percentage, this.settings.animationDuration));
         };
     }
 
     render() {
         return(
-            React.createElement("div", {className: "react-svg-doughnut", style: Object.assign({}, this.styles.svgStyle, {position: 'relative'})}, 
-                React.createElement("svg", {viewBox: `0 0 ${this.size} ${this.size}`}, 
+            React.createElement("div", {className: "react-svg-doughnut", style: Object.assign({}, this.settings.styles.svgStyle, {position: 'relative'})}, 
+                React.createElement("svg", {viewBox: `0 0 ${this.settings.size} ${this.settings.size}`}, 
                     React.createElement("circle", {
                         cx: "50%", 
                         cy: "50%", 
-                        r: (this.size / 2) - this.props.settings.thickness, 
-                        style: Object.assign({}, this.styles.sharedStyles , this.styles.circleStyle)}
+                        r: (this.settings.size / 2) - this.settings.thickness, 
+                        style: Object.assign({}, this.settings.styles.sharedStyles , this.settings.styles.circleStyle)}
                     ), 
                     React.createElement("path", {
-                        d: calculatePathShape(this.state.appliedPercentage, this.size, this.props.settings.thickness), 
-                        style: Object.assign({}, this.styles.sharedStyles , this.styles.pathStyle)}
+                        d: calculatePathShape(this.state.appliedPercentage, this.settings.size, this.settings.thickness), 
+                        style: Object.assign({}, this.settings.styles.sharedStyles , this.settings.styles.pathStyle)}
                     )
                 ), 
-                React.createElement("div", {style: Object.assign({}, this.styles.textWrapperStyle)}, 
-                    React.createElement("p", {style: Object.assign({}, this.styles.percentageTextStyle)}, `${this.state.appliedPercentage}%`), 
-                    this.labelText && React.createElement("p", {style: Object.assign({}, this.styles.labelTextStyle)}, this.labelText)
+                React.createElement("div", {style: Object.assign({}, this.settings.styles.textWrapperStyle)}, 
+                    React.createElement("p", {style: Object.assign({}, this.settings.styles.percentageTextStyle)}, `${this.state.appliedPercentage}%`), 
+                    this.settings.labelText && React.createElement("p", {style: Object.assign({}, this.settings.styles.labelTextStyle)}, this.settings.labelText)
                 )
             )
         );
--- src/index.jsx
+++ src/index.jsx
@@ -8,59 +8,51 @@ class SvgDoughnut extends React.Component {
 
     constructor(props) {
         super(props);
-
         this.state = {
             appliedPercentage: 0
         };
-
-        this.styles = generateStyles(this.props.settings);
-        this.size = this.props.settings.size || 150;
-        this.percentage = this.props.percentage || 0;
-        this.animationDuration = this.props.settings.animationDuration;
-        this.labelText = this.props.settings.labelText;
         this.settings = configure(this.props.settings);
-        console.log(this.settings);
     }
 
     componentDidMount() {
-        if (this.animationDuration || this.animationDuration > 0) {
+        if (this.settings.animationDuration || this.settings.animationDuration > 0) {
             this.animate();
         } else {
             this.setState({
-                appliedPercentage: this.percentage
+                appliedPercentage: this.settings.percentage
             });
         }
     }
 
     animate() {
         const self = this;
-        for(let i = 0; i < (this.percentage + 1); i++){
+        for(let i = 0; i < (this.settings.percentage + 1); i++){
             setTimeout(() => {
                 self.setState({
                     appliedPercentage: i
                 });
-            }, i * stepDuration(this.percentage, this.animationDuration));
+            }, i * stepDuration(this.settings.percentage, this.settings.animationDuration));
         };
     }
 
     render() {
         return(
-            <div className="react-svg-doughnut" style={Object.assign({}, this.styles.svgStyle, {position: 'relative'})}>
-                <svg viewBox={`0 0 ${this.size} ${this.size}`}>
+            <div className="react-svg-doughnut" style={Object.assign({}, this.settings.styles.svgStyle, {position: 'relative'})}>
+                <svg viewBox={`0 0 ${this.settings.size} ${this.settings.size}`}>
                     <circle
                         cx="50%"
                         cy="50%"
-                        r={(this.size / 2) - this.props.settings.thickness}
-                        style={Object.assign({}, this.styles.sharedStyles , this.styles.circleStyle )}>
+                        r={(this.settings.size / 2) - this.settings.thickness}
+                        style={Object.assign({}, this.settings.styles.sharedStyles , this.settings.styles.circleStyle )}>
                     </circle>
                     <path
-                        d={calculatePathShape(this.state.appliedPercentage, this.size, this.props.settings.thickness)}
-                        style={Object.assign({}, this.styles.sharedStyles , this.styles.pathStyle )}>
+                        d={calculatePathShape(this.state.appliedPercentage, this.settings.size, this.settings.thickness)}
+                        style={Object.assign({}, this.settings.styles.sharedStyles , this.settings.styles.pathStyle )}>
                     </path>
                 </svg>
-                <div style={Object.assign({}, this.styles.textWrapperStyle )}>
-                    <p style={Object.assign({}, this.styles.percentageTextStyle)} >{`${this.state.appliedPercentage}%`}</p>
-                    {this.labelText && <p style={Object.assign({}, this.styles.labelTextStyle)}>{this.labelText}</p>}
+                <div style={Object.assign({}, this.settings.styles.textWrapperStyle )}>
+                    <p style={Object.assign({}, this.settings.styles.percentageTextStyle)} >{`${this.state.appliedPercentage}%`}</p>
+                    {this.settings.labelText && <p style={Object.assign({}, this.settings.styles.labelTextStyle)}>{this.settings.labelText}</p>}
                 </div>
             </div>
         );
